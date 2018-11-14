708493106a3646106478122ed5ef90854436d133
PA <pierreantoine.pelerin@gmail.com>
2018-11-14 10:09:19 +0100

    premier

---
+++ com.fr.eseo.TPTest/src/main/java/tp_Maven2018/com/fr/eseo/TPTest/App.java
@@ -0,0 +1,13 @@
+package tp_Maven2018.com.fr.eseo.TPTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
